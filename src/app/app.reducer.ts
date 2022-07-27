fa7efba2bd18571323b5ed972ba821c6b049ec60
ekremucuncu <59807398+ekremucuncu@users.noreply.github.com>
2022-07-27 16:35:36 +0300

    solved some Problems

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,5 +1,4 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store'
-<<<<<<< HEAD
 import * as fromAuth from './shared/store/auth/auth.reducer'
 import * as fromLoading from './shared/store/loading/loading.reducer'
 import * as fromProduct from './shared/store/products/products.reducer'
@@ -15,23 +14,6 @@ export const reducers: ActionReducerMap<State> ={
     auth:fromAuth.authenticationReducer,
     loading:fromLoading.loadingReducer,
     product:fromProduct.productRecuders
-=======
-import * as fromAuth from './shared/reducers/auth.reducer'
-import * as fromLoading from './shared/reducers/loading.reducer'
-import * as fromPhones from './shared/reducers/phones/phones.reducer'
-
-export interface State {
-  auth: fromAuth.State,
-  loading: fromLoading.State,
-  phones: fromPhones.State
-}
-
-
-export const reducers: ActionReducerMap<State> = {
-  auth: fromAuth.authenticationReducer,
-  loading: fromLoading.loadingReducer,
-  phones: fromPhones.phonesReducer
->>>>>>> 544149201b588bfffcf8d6ee14362ecc25c4a210
 }
 
 export const getAuthState = createFeatureSelector<fromAuth.State>('auth');
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Accessoir } from '../shared/models/accessoirs.model';
 import { Iphone } from '../shared/models/iphone.model';
 import { ProductService } from '../core/services/products.service'
-<<<<<<< HEAD
 import { iPhones,Accessoirs,Offers } from '../shared/firestore.collections';
 import { Store } from '@ngrx/store';
 import * as fromRoot from '../app.reducer'
@@ -10,13 +9,6 @@ import * as productActions from '../shared/store/products/products.actions'
 import { Offer } from '../shared/models/offers.model';
 
 
-=======
-import { take } from 'rxjs/operators'
-import { iPhones, Accessoirs, Offers } from '../shared/firestore.collections';
-import { Store } from '@ngrx/store';
-import * as fromApp from '../app.reducer';
-import * as phonesActions from '../shared/reducers/phones/phones.actions';
->>>>>>> 544149201b588bfffcf8d6ee14362ecc25c4a210
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -24,7 +16,6 @@ import * as phonesActions from '../shared/reducers/phones/phones.actions';
 })
 export class ProductsComponent implements OnInit {
 
-<<<<<<< HEAD
 
  
   @Input() from:string;
@@ -65,36 +56,6 @@ export class ProductsComponent implements OnInit {
         this.loading=result.offersLoading
       })
       this.product=Offers
-=======
-  @Input() from: string;
-  product: string;
-  managing: boolean;
-  fetchedProducts: Iphone[] | Accessoir[];
-
-  constructor(
-    private productservice: ProductService,
-    private store: Store<fromApp.State>
-  ) { }
-
-  ngOnInit(): void {
-    if (this.from.includes('iphone')) {
-      this.store.dispatch(phonesActions.getPhonesStarted());
-      this.store.select('phones').subscribe((result) => {
-        this.fetchedProducts = result.phones;
-      })
-      // this.productservice.fetchProducts('iPhones');
-      // this.product = iPhones
-    }
-    else if (this.from.includes('accessoir')) {
-      this.productservice.fetchProducts('Accessoirs');
-      this.product = Accessoirs
-    }
-    else if (this.from.includes('offer')) {
-      this.productservice.fetchProducts('Offers');
-      this.productservice.fetchProducts('Accessoirs');
-      this.productservice.fetchProducts('iPhones');
-      this.product = Offers
->>>>>>> 544149201b588bfffcf8d6ee14362ecc25c4a210
     }
 
     if (this.from.includes('manager')) {
@@ -103,17 +64,6 @@ export class ProductsComponent implements OnInit {
     else {
       this.managing = false
     }
-<<<<<<< HEAD
     
-=======
-
-    // this.productservice.getProduct(this.product)
-    //   .pipe(take(1))
-    //   .subscribe(
-    //     productlist => {
-    //       this.fetchedProducts = productlist
-    //     }
-    //   )
->>>>>>> 544149201b588bfffcf8d6ee14362ecc25c4a210
   }
 }
