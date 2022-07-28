da66d592ef945d72c956d11a4463c8a7d59eb5b3
ekremucuncu <59807398+ekremucuncu@users.noreply.github.com>
2022-07-28 14:57:38 +0300

    for fetching after updated product

--- src/app/products/productmanager/productmanager.component.ts
+++ src/app/products/productmanager/productmanager.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Accessoir } from 'src/app/shared/models/accessoirs.model';
+import { Iphone } from 'src/app/shared/models/iphone.model';
+import { Offer } from 'src/app/shared/models/offers.model';
+import * as fromRoot from '../../app.reducer'
+
 
 @Component({
   selector: 'app-productmanager',
@@ -9,17 +15,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProductmanagerComponent implements OnInit {
 
   from:string;
+  fetchedProducts:Iphone[] | Accessoir[] | Offer[]
 
   constructor(
     private route:ActivatedRoute,
     private router:Router,
+    private store:Store<fromRoot.State>
   ) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   ngOnInit(): void {
     this.from=this.route.snapshot.params['product']+'manager';
-
+    this.store.select('product').subscribe((result)=>{
+      this.fetchedProducts = result.iphones
+    })
   }
 
 }
